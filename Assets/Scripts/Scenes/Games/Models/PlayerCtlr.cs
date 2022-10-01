0d19a86aa124af0cdd298f75ce9b16d656b59777
dyguests <dyguests@qq.com>
2022-10-02 00:24:04 +0800

    player input handle.

---
+++ Assets/Scripts/Scenes/Games/Models/PlayerCtlr.cs
@@ -0,0 +1,78 @@
+using System;
+using Scenes.Games.Physicses;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Scenes.Games.Models
+{
+    public class PlayerCtlr : PhysicsObject, IPlayerInputActions
+    {
+        [Space] [SerializeField] private SpriteRenderer sr;
+
+        #region Define Variants
+
+        // --------------- move ---------------
+
+        [Space] [SerializeField] private float moveSpeed = 9f;
+        [SerializeField] private float moveAccelerationTime = 6 * 0.02f;
+        private float moveAcceleration;
+        [SerializeField] private float moveDecelerationTime = 3 * 0.02f;
+        private float moveDeceleration;
+
+        // --------------- jump ---------------
+
+        [SerializeField] private float jumpHeight = 3.2f;
+        private float jumpSpeed;
+        [SerializeField] private float jumpSpoolTime = 3 * 0.02f;
+        [SerializeField] private float jumpCoyoteTime = 3 * 0.02f;
+        [SerializeField] private int jumpMaxTimes = 1;
+        private int jumpRemainingTimes = 1;
+        [SerializeField] private float jumpCost = 25f;
+        [SerializeField] private AnimationCurve jumpInterpolator;
+
+        #endregion
+
+        #region Input Variants
+
+        private Vector2 moveInput;
+
+        private bool jumpInput;
+        private float lastJumpDownInputTime = float.MinValue;
+
+        #endregion
+
+        public override Vector2 Velocity { get; set; }
+
+        protected override void HandleGraphics() { }
+
+        protected override void HandleKinematics() { }
+
+        protected override void UpdateGrounded(bool grounded) { }
+
+        public void HandleMoveInput(InputAction.CallbackContext ctx)
+        {
+            Debug.Log("HandleMoveInput ctx:" + ctx);
+            if (ctx.phase == InputActionPhase.Performed || ctx.phase == InputActionPhase.Canceled)
+            {
+                var value = ctx.ReadValue<Vector2>();
+                Debug.Log("HandleMoveInput value:" + value);
+                moveInput = value;
+            }
+        }
+
+        public void HandleJumpInput(InputAction.CallbackContext ctx)
+        {
+            Debug.Log("HandleJumpInput ctx:" + ctx);
+            if (ctx.phase == InputActionPhase.Started || ctx.phase == InputActionPhase.Canceled)
+            {
+                var value = ctx.ReadValue<float>();
+                var jump = Math.Abs(value - 1) < 0.001f;
+                jumpInput = jump;
+                if (jump) lastJumpDownInputTime = Time.fixedTime;
+                Debug.Log("HandleJumpInput jump:" + jump);
+            }
+        }
+    }
+
+    public interface IPlayerInputActions { }
+}
---
+++ Assets/Scripts/Scenes/Games/Physicses/PhysicsObject.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+
+namespace Scenes.Games.Physicses
+{
+    public abstract class PhysicsObject : MonoBehaviour
+    {
+        #region Constants
+
+        private const float MinMoveDistance = 0.001f;
+        private const float ShellRadius = 0.01f;
+        private const float MinGroundNormalY = .65f;
+
+        #endregion
+
+        [SerializeField] protected Rigidbody2D rb;
+        [SerializeField] protected Collider2D cd;
+
+        #region Define Variants
+
+        private ContactFilter2D contactFilter;
+
+        #endregion
+
+        #region Calculate Variants
+
+        private readonly RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+
+        public abstract Vector2 Velocity { get; set; }
+        public bool Grounded { get; private set; }
+
+        private Vector2 groundNormal;
+
+        #endregion
+
+        protected virtual void Start()
+        {
+            contactFilter.useTriggers = false;
+            contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(rb.gameObject.layer));
+            contactFilter.useLayerMask = true;
+        }
+
+        protected virtual void Update()
+        {
+            HandleGraphics();
+        }
+
+        protected abstract void HandleGraphics();
+
+        private void FixedUpdate()
+        {
+            HandleKinematics();
+            ApplyMovement();
+        }
+
+        protected abstract void HandleKinematics();
+
+        private void ApplyMovement()
+        {
+            var deltaPosition = Velocity * Time.fixedDeltaTime;
+            var moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x);
+
+            var grounded = false;
+            var movement = moveAlongGround * deltaPosition.x;
+            Move(movement, false, ref grounded);
+            movement = Vector2.up * deltaPosition.y;
+            Move(movement, true, ref grounded);
+
+            this.Grounded = grounded;
+            UpdateGrounded(grounded);
+        }
+
+        protected abstract void UpdateGrounded(bool grounded);
+
+        private void Move(Vector2 movement, bool yMovement, ref bool grounded)
+        {
+            var distance = movement.magnitude;
+            if (distance < MinMoveDistance) return;
+            int count = cd.Cast(movement, contactFilter, hitBuffer, distance + ShellRadius);
+            for (int i = 0; i < count; i++)
+            {
+                Vector2 currentNormal = hitBuffer[i].normal;
+                if (currentNormal.y > MinGroundNormalY)
+                {
+                    grounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(Velocity, currentNormal);
+                if (projection < 0)
+                {
+                    Velocity = Velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBuffer[i].distance - ShellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+
+            rb.position += movement.normalized * distance;
+        }
+    }
+}
